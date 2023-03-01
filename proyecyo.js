9998b48e571cf6e749c5095dde63dfbfc7bad638
roberta zuccali <rozuccali@gmail.com>
2023-03-01 02:08:00 -0300

    Proyecto final

---
+++ proyecyo.js
@@ -0,0 +1,7 @@
+function mostrartelefono () { 
+    document.getElementById("contactar").innerHTML="0341-5142371"
+}
+
+function mostrarmail () { 
+    document.getElementById("correo").innerHTML="rozuccali@gmail.com"
+}
